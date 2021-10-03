51642fa3f125b35f54c9e0ab58bebdd7e0a6429a
Kokipoki <karimgame1999@yahoo.com>
2021-10-03 19:54:48 -0400

    Text Classification Bar Chart

--- Assignment 1/TextClassification.py
+++ Assignment 1/TextClassification.py
@@ -1,1 +1,14 @@
-
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+classifications = ['Business', 'Entertainment', 'Politics', 'Sport', 'Tech']
+instances = [len(os.listdir("BBC/business")),
+                len(os.listdir("BBC/entertainment")),
+                len(os.listdir("BBC/politics")),
+                len(os.listdir("BBC/sport")),
+                len(os.listdir("BBC/tech"))]
+plt.bar(classifications, instances)
+plt.xlabel('Classification')
+plt.ylabel('Instances')
+plt.title('BBC Distribution')
+plt.savefig("BBC-distribution.pdf")
