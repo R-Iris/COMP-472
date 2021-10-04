330adafdaaf865a40b8686b09bc88eb15b770fad
Kokipoki <karimgame1999@yahoo.com>
2021-10-04 15:57:15 -0400

    Comments

--- Assignment 1/TextClassification.py
+++ Assignment 1/TextClassification.py
@@ -27,11 +27,15 @@ plt.title('BBC Distribution')
 
 # Loading the corpus
 data = sklearn.datasets.load_files("BBC", encoding='latin1', load_content=True)
+# data is a bunch of 5 attributes, data, target, target_names, DESCR, filesnames
+# The data attribute is a list of strings, with each string being the raw content of a file
 
 # Creates a dictionary of the vocabulary of the corpus
 vectorizer = fe.text.CountVectorizer()
+# Count vectorizer seperates the words in each file into their own strings
 vectorizer.fit_transform(data.data)
 vector = vectorizer.transform(data.data)
+# Vector is the count of each word in the format of a 2d array
 # ft = vectorizer.get_feature_names_out()  # Gets the feature names, output is an array[5]
 
 # ! Proof that CountVectorizer() stores their words and instances properly !
@@ -39,6 +43,8 @@ vector = vectorizer.transform(data.data)
 # print(vector.toarray())  # Prints an array
 
 doc_train, doc_test, target_train, target_test = me.train_test_split(vector.toarray(), data.target, test_size=0.2, train_size=0.8, random_state=None)
+# train_test_split splits a list into 2, in this case, 2 arguments return 4 lists
+# doc is the words and the count of their instances, target is their class (category, file directory)
 
 # ! Proof that the split is done properly !
 print("Total number of document: ",
@@ -57,14 +63,16 @@ MultiNB.fit(doc_train, target_train)
 
 # Evaluating the MultiNB to the test set
 predictions = MultiNB.predict(doc_test)
-# print(predictions)  # This part sucks, makes me want to kill myself. What am I even doing?
-# try print(str(predictions)) and dont kys :) - Ryan
+# predictions is a 2d array of the predicted target values
+# print(str(predictions))
+
 # Writing to a file and overwriting the content
 file = open("bbc-performance.txt", "w")
 file.write("(a) ***  MultinomialNB default values, try 1 ***\n\n")
 
 # Creating the confusion matrix
 confusionMatrix = sklearn.metrics.confusion_matrix(target_test, predictions)
+# 2d array of the confusion matrix
 print(confusionMatrix)
 
 file.write("(b)\n")
@@ -78,14 +86,21 @@ print(confusionMatrix)
 
 # Obtaining precision, recall and f1-measure of the predictions
 precRecF1 = met.classification_report(target_test, predictions)
+# the classification report returns a "report" or a dictionary (refer to online documentation)
 print("\n(c)\n")
 print("\tPrecision, Recall, and F1-measure: ")
 print(precRecF1)
 
 # Accuracy, macro-average F1 and weighted-average F1 of the predictions
 accuracy = met.accuracy_score(target_test, predictions)
+# returns a float
+
 macroF1 = met.f1_score(target_test, predictions, average='macro')
+# returns a float
+
 weightedF1 = met.f1_score(target_test, predictions, average='weighted')
+# returns a float
+
 print("\n(d)\n")
 print("\tAccuracy: ")
 print("\t" + str(accuracy))
@@ -101,6 +116,7 @@ print("\tPrior Probabilities: ")
 ignore = "README.TXT"
 classes = [e for e in os.listdir("BBC") if e not in ignore]
 
+# NEEDS TO BE FIXED
 for e in classes:
     print("\t" + e + " class prior probability : " + str(1/len(classes)))
 
