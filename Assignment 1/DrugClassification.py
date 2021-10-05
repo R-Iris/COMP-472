a715e52139375b4c222e1a4dfac1d7561df15c1f
Iris <iris.radu@hotmail.com>
2021-10-05 19:55:04 -0400

    Fixed train_test_split

--- Assignment 1/DrugClassification.py
+++ Assignment 1/DrugClassification.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import sklearn.naive_bayes
 from sklearn.model_selection import train_test_split
+import numpy as np
 
 # Reading the csv file and printing it to the console
 document = pd.read_csv("drug200.csv")
-print(document)
+# print(document)
 
 # Plotting the distribution of the instances in each class and storing the graph in a file
 plt.hist(document.Drug, bins=range(6), edgecolor="black", align="left")
@@ -17,15 +19,26 @@ plt.savefig("drug-distribution.pdf")
 # Nominal features will be categorized as followed: 0 = FEMALE, 1 = MALE
 document.BP = pd.Categorical(document.BP, ["LOW", "NORMAL", "HIGH"], ordered=True)
 document.BP = document.BP.cat.codes
-print(document.BP)
+# print(document.BP)
 
 document.Cholesterol = pd.Categorical(document.Cholesterol, ["LOW", "NORMAL", "HIGH"], ordered=True)
 document.Cholesterol = document.Cholesterol.cat.codes
-print(document.Cholesterol)
+# print(document.Cholesterol)
 
 document.Sex = pd.Categorical(document.Sex, ["F", "M"])
 document.Sex = document.Sex.cat.codes
-print(document.Sex)
+# print(document.Sex)
+# print(document.Drug)
+# print(document.Age.array)
+allDocuments = np.array(document)
+# print(allDocuments)
 
 # The default split size in the sklearn model is 75% training and 25% testing data split
-train_test_split(document.Age, document.Sex, document.BP, document.Cholesterol, document.Na_to_K, document.Drug)
+# doc_train, doc_test, target_train, target_test = train_test_split(allDocuments, document.Drug)
+# print(doc_train)
+doc_train, doc_test, target_train, target_test = train_test_split(allDocuments, document.Drug)
+# print(doc_train)
+
+# Gaussian Naive Bayes Classifier
+# Accepts an array-like of prior probabilities of classes
+GaussianNB = sklearn.naive_bayes.GaussianNB()
