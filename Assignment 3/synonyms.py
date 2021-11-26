0f5e198b4862d69270cae4b46d97dd9cb439fa2a
Iris <iris.radu@hotmail.com>
2021-11-26 18:51:53 -0500

    Try catch block

--- Assignment 3/synonyms.py
+++ Assignment 3/synonyms.py
@@ -8,6 +8,18 @@ with open("synonyms.csv", newline='') as synonymsCSV:
     next(reader)
     for line in reader:
         query = []
+        maxCosine = 0
+        bestSyn = ""
+        questionWord = line[0]
+        answerWords = line[2]
+
+        query.append(questionWord,answerWords[0])
+        query.append(questionWord,answerWords[0])
+        query.append(questionWord,answerWords[0])
+        query.append(questionWord,answerWords[0])
+        
+
+
         couple1 = (line[0], line[2])
         query.append(couple1)
         couple2 = (line[0], line[3])
@@ -16,7 +28,26 @@ with open("synonyms.csv", newline='') as synonymsCSV:
         query.append(couple3)
         couple4 = (line[0], line[5])
         query.append(couple4)
-        # compare = sim.Similarity(query, googleW2VModel)
+
+        for w1, w2 in query:
+            try:
+                wordNotPresent = googleW2VModel[w1]
+
+            except KeyError:
+                print("The word(s) does not appear in this model")
+
+            currentCosine = googleW2VModel.similarity(w1, w2)
+            if maxCosine < currentCosine:
+                maxCosine = currentCosine
+                bestSyn = w2
+
+        print(w1 + " || " + bestSyn)
+
+        if bestSyn == line[1]:
+            print("Correct")
+        else:
+            print("Wrong")
+            #print('%r\t%r\t%.2f' % (w1, w2, googleW2VModel.similarity(w1, w2)))
 
 
 
